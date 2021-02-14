01dc025198ac0f74807e81eecee91c282e7de81f
Ayat-Ayyad <ayyadayat89@gmail.com>
2021-02-14 14:58:35 +0200

    socket assignment

---
+++ the_handshake/server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const server = app.listen(8000, () =>
+console.log('The server is all fired up on port 8000')
+);
+
+const io = require('socket.io')(server, { cors: true });
+
+io.on("connection", socket => {
+    console.log(socket.id);
+    console.log("Nice to meet you. (shake hand)");
+    socket.on("event_from_client", data => {
+        socket.emit("Nice to meet you.", data);
+        
+    })
+})
+
